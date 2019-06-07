37cab12acc7ef5c2c0c2a43d3b5b285be6c5c085
csxiaocy <xiaochuyu@outlook.com>
2019-06-07 13:09:48 +0800

    user表、validation表初始化

--- config/config.default.js
+++ config/config.default.js
@@ -10,7 +10,23 @@ module.exports = appInfo => {
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
-  const config = exports = {};
+  const config = {
+    security: {
+      csrf: {
+        enable: false,
+      },
+    },
+
+    // mysql配置
+    sequelize: {
+      dialect: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'xiaocy',
+      database: 'test',
+    },
+  };
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1559744036638_5118';
