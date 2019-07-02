86c3320ee601ea148940660f657468f92f5c5828
csxiaocy <xiaochuyu@outlook.com>
2019-07-02 16:32:38 +0800

    forget to add errCode.js

--- config/errCode.js
+++ config/errCode.js
@@ -1,14 +1,45 @@
 'use strict';
 
+// errCode拼接
+// obj格式:
+// const obj = {code:'01', a:{code:'02',b:'03'} }
+const errCodeJoin = obj => {
+  for (const item in obj) {
+    if (obj.hasOwnProperty(item) && item !== 'code') {
+      obj[item] = new Proxy(obj[item], {
+        get(target) {
+          return `${obj.code}${target.code}${Reflect.get(...arguments)}`;
+        },
+      });
+    }
+  }
+};
+
 const errCode = {
-  UnknownErr: '100000',
-  User: {
-    registryAccountParamErr: '010001',
-    registryRepassDiff: '010002',
-    registryAccountExisted: '010003',
-    loginAccountNoexist: '010004',
-    loginPasswordError: '010005',
+  code: '01',
+  a: {
+    code: '02',
+    b: '03',
+    c: '04',
+  },
+  d: {
+    code: '05',
+    e: '06',
   },
 };
 
-module.exports = errCode;
+errCodeJoin(errCode);
+
+console.log(errCode.a.b);
+
+// const errCode = {
+//   UnknownErr: '100000',
+//   User: {
+//     registryAccountParamErr: '010001',
+//     registryAccountExisted: '010003',
+//     loginAccountNoexist: '010004',
+//     loginPasswordError: '010005',
+//   },
+// };
+//
+// module.exports = errCode;
