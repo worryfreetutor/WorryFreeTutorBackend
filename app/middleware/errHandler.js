64c1f1a62260b08e309dd3f799fac8fc1d11ec1f
csxiaocy <xiaochuyu@outlook.com>
2019-07-11 22:54:21 +0800

    404

--- app/middleware/errHandler.js
+++ app/middleware/errHandler.js
@@ -3,11 +3,10 @@
 module.exports = () => {
   return async function errorHandler(ctx, next) {
     ctx.set('Content-Type', 'application/json');
-    // ctx.status = 404;
     try {
       await next();
       const res = ctx.body;
-      // ctx.status = 200;
+      ctx.status = ctx.status === 404 ? 404 : 200;
       ctx.body = {
         code: '0',
         ...res,
@@ -17,7 +16,7 @@ module.exports = () => {
         code: err.code,
         message: err.message ? err.message : err.toString(),
       };
-      // ctx.status = 200;
+      ctx.status = 200;
       ctx.body = JSON.stringify(body);
       // 所有的异常都在 app 上触发一个 error 事件
       ctx.app.emit('error', err, ctx);
