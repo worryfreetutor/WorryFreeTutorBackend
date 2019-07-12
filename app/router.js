dc28abbce4b92fce70092e9d6e93703a53cd280b
csxiaocy <xiaochuyu@outlook.com>
2019-07-13 01:00:48 +0800

    user info

--- app/router.js
+++ app/router.js
@@ -10,4 +10,8 @@ module.exports = app => {
   router.post('/user/register', controller.user.register);
   router.post('/user/login', controller.user.login);
   router.get('/user/refresh', middleware.getAccount(), controller.user.refresh);
+  // 更新用户信息
+  router.post('/user/update', middleware.getAccount(), controller.user.updateInfo);
+  // 获取用户具体信息
+  router.get('/user/info', middleware.getAccount(), controller.user.getInfo);
 };
--- app/service/user.js
+++ app/service/user.js
@@ -34,8 +34,63 @@ class UserService extends Service {
   }
 
   /**
-   * 完善用户信息
+   * 更新用户信息
    */
+  async updateUserInfo(account, nickname, sex, grade, intro) {
+    const { ctx } = this;
+    // 将sex转换为大写
+    if (sex) {
+      sex = sex.toUpperCase();
+    }
+    const options = {
+      nickname,
+      sex,
+      grade,
+      intro,
+    };
+    for (const index in options) {
+      if (!options[index]) {
+        delete options[index];
+      }
+    }
+    if (JSON.stringify(options) === '{}') {
+      throw ctx.helper.createError('无更新项', userErrCode.updateInfo.noUpdateItem);
+    }
+    try {
+      await ctx.model.User.update(options, {
+        where: {
+          account,
+        },
+      });
+    } catch (err) {
+      ctx.logger.warn(err);
+      throw ctx.helper.createError(`[service/user.js 更新用户] ${err.toString()}`);
+    }
+    return {
+      message: '更新成功',
+    };
+  }
+
+  /**
+   * 获取用户信息
+   */
+  async getUserInfo(account) {
+    const { ctx } = this;
+    let userInfo;
+    try {
+      userInfo = await ctx.model.User.findOne({
+        attributes: [ 'account', 'nickname', 'avatar', 'sex', 'grade', 'intro', 'is_teacher', 'is_student' ],
+        where: { account },
+      });
+    } catch (err) {
+      ctx.logger.warn(err);
+      throw ctx.helper.createError(`[service/user.js 获取用户信息] ${err.toString()}`);
+    }
+    const data = userInfo.dataValues;
+    return {
+      data,
+    };
+  }
 }
 
 module.exports = UserService;
